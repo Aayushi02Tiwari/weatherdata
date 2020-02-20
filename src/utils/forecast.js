57241758382611ba1cac7299511358a2fda14510
Aayushi <aayushi02tiwari@gmail.com>
2020-02-20 12:08:37 +0530

    Add high to low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,8 @@ const forecast = (latitude, longitude, callback) => {
         else {
             // console.log('It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain')
             // console.log(response.body.timezone)
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. This hight today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain')
         }
     })
 
